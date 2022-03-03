44aaa021c63ec7903f4644f37b91f94ae0844e75
Daniel Zielasko <daniel.zielasko@rwth-aachen.de>
2022-03-03 15:20:57 +0100

    no message

---
+++ Assets/Scripts/Locomotion/Steering/Inputs/MYscript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MYscript : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.position = new Vector3(3,2,1);
+    }
+}
